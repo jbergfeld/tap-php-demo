28498eeb09a9f8d12925422263604de28c707b39
Jon Bergfeld <jbergfeld@pivotal.io>
2022-04-13 21:41:19 +0000

    replaced with HTML

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,3 +1,10 @@
-<?php
-  phpinfo();
-?>
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>Hello PHP</title>
+ </head>
+ <body>
+  <h1>Hello PHP</h1>
+  <?php echo "This is a simple PHP web site."; ?>
+ </body>
+</html>
